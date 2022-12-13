1c19c5a1e9a7b462831105c85e27bc2e5ba3e0f5
EuAndersonRoberto <euandersondigital@gmail.com>
2022-12-13 02:30:12 -0300

    Atualização da main

--- main.go
+++ main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"GoAppWeb/db"
+	"GoAppWeb/models"
 	"net/http"
 	"text/template"
 )
@@ -23,35 +23,6 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-
-	db := db.ConectaComBancoDeDados()
-
-	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
-	if err != nil {
-		panic(err.Error())
-	}
-
-	p := Produto{}
-	produtos := []Produto{}
-
-	for selectDeTodosOsProdutos.Next() {
-		var id, quantidade int
-		var nome, descricao string
-		var preco float64
-
-		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
-		if err != nil {
-			panic(err.Error())
-		}
-
-		p.Nome = nome
-		p.Descricao = descricao
-		p.Preco = preco
-		p.Quantidade = quantidade
-
-		produtos = append(produtos, p)
-	}
-
-	temp.ExecuteTemplate(w, "Index", produtos)
-	defer db.Close()
+	todosOsProdutos := models.BuscaTodosOsProdutos()
+	temp.ExecuteTemplate(w, "Index", todosOsProdutos)
 }
